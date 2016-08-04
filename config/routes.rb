eeff87d7d1408d5e08464603a12886c6aa01f61b
Brett Birth <brett.birth@gmail.com>
2016-08-03 20:53:18 -0500

    adds sorting by category and brand

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root 'store_front#all_items'
   
-  get 'all' => 'store_front/all_items'
-  get 'categorical' => 'store_front/items_by_category'
-  get 'branding' => 'store_front/items_brand'
+  get 'all' => 'store_front#all_items'
+  get 'categorical' => 'store_front#items_by_category'
+  get 'branding' => 'store_front#items_brand'
+
+
 
   devise_for :users
   resources :categories
